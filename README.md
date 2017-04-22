0068854d5fede44a83b4158cacd4aaea53a16cd1
arnaucode <idoctnef@openmailbox.org>
2017-04-22 16:39:47 +0200

    implemented text generation with markov chains

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # goMarkov
 markov chains text generator written in Go from scratch
+
+
+```go
+states := markov.train(text)
+generatedText := markov.generateText(states, firstWord, count)
+fmt.Println(generatedText)
+```
+(in the text variable, goes the text content, can be loaded from a .txt file)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func readText(path string) (string, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		//Do something
+	}
+	dataClean := strings.Replace(string(data), "\n", " ", -1)
+	content := string(dataClean)
+	return content, err
+}
+
+func main() {
+	fmt.Print("entry the first word: ")
+	newcommand := bufio.NewReader(os.Stdin)
+	firstWord, _ := newcommand.ReadString('\n')
+	firstWord = strings.TrimSpace(firstWord)
+	fmt.Print("first word: ")
+	fmt.Println(firstWord)
+
+	fmt.Println("how many words you want on the text?")
+	newcommand = bufio.NewReader(os.Stdin)
+	answer, _ := newcommand.ReadString('\n')
+	answer = strings.TrimSpace(answer)
+	fmt.Print("Number of words on text to generate: ")
+	fmt.Println(answer)
+	count, err := strconv.Atoi(answer)
+	if err != nil {
+		fmt.Println("incorrect entry, need a positive number")
+	}
+
+	text, _ := readText("text.txt")
+
+	fmt.Println("generating text")
+	states := markov.train(text)
+	generatedText := markov.generateText(states, firstWord, count)
+	fmt.Println("")
+	fmt.Println(generatedText)
+}
